12f512aa17445f5c20383b17ea2522fba2e99aee
c2r0b <c2r0b@proton.me>
2024-04-05 11:09:16 +0200

    drawer dark mode fix

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -2,32 +2,15 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 import '../dialogs/notification_dialog.dart';
 
-class DrawerWidget extends StatefulWidget {
+class DrawerWidget extends StatelessWidget {
   const DrawerWidget({super.key});
 
   @override
-  _DrawerWidgetState createState() => _DrawerWidgetState();
-}
-
-class _DrawerWidgetState extends State<DrawerWidget> {
-  @override
   Widget build(BuildContext context) {
-    //FlutterLocalNotificationsPlugin plugin =FlutterLocalNotificationsPlugin(); // Initialize the plugin
-
     return Drawer(
-      // Add a ListView to the drawer. This ensures the user can scroll
-      // through the options in the drawer if there isn't enough vertical
-      // space to fit everything.
       child: ListView(
-        // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
+        padding: const EdgeInsets.only(top: 40, left: 5),
         children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.surface,
-            ),
-            child: const Text('Spazzamento Toscana'),
-          ),
           ListTile(
             title: const Text('Come funziona'),
             leading: Icon(Icons.help_outline,
